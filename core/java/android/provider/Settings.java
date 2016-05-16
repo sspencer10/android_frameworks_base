3eb83b2e48c9e6d5ae4fd4ab459eecdfeaeae572
Nico60 <ndhouailly@gmail.com>
2016-05-16 21:48:30 +0200

    Add animations to quick settings tiles [1/2]
    mydongistiny: Brought from nougat->8.1
    
    Brought to pie by ZeNiXxX
    
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4917,7 +4917,21 @@ public final class Settings {
          */
         public static final String FP_UNLOCK_KEYSTORE = "fp_unlock_keystore";
 
-         /**
+        /**
+         * Change quick settings tiles animation style
+         *
+         * @hide
+         */
+        public static final String ANIM_TILE_STYLE = "anim_tile_style";
+
+        /**
+         * Change quick settings tiles animation duration
+         *
+         * @hide
+         */
+        public static final String ANIM_TILE_DURATION = "anim_tile_duration";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
