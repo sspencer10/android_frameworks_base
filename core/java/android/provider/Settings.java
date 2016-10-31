a5e9ba8561eee56a900f013092ff4509b8207d88
maxwen <max.weninger@gmail.com>
2016-10-31 23:48:50 +0100

    SystemUI: make QS columns count configurable
    And add infrastructure to add more settings and
    distribute them to QS panel
    
    PS4
    we must explicitly hide the columns submenu
    
    base: SystemUI: add column count 6
    
    now that we have hide label
    
    base: SystemUI: qs colums menu with checkboxes
    
    base: SystemUI add landscape qs columns config
    
    allow max 9 columns in landscape
    
    Change-Id: I401cca347da1a94f352a9ffcb99f1409ddc4d008
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4518,6 +4518,17 @@ public final class Settings {
         public static final String USE_BOTTOM_GESTURE_NAVIGATION = "use_bottom_gesture_navigation";
 
         /**
+         * Number of qs columns on landscape orientation
+         * @hide
+         */
+        public static final String QS_LAYOUT_COLUMNS_LANDSCAPE = "qs_layout_columns_landscape";
+
+        /**
+         * @hide
+         */
+        public static final String QS_LAYOUT_COLUMNS = "qs_layout_columns";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
--- packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
@@ -122,6 +122,7 @@ public class QSPanel extends LinearLayout implements Tunable, Callback, Brightne
         mPanelPageIndicator = (PageIndicator) LayoutInflater.from(context).inflate(
                 R.layout.qs_page_indicator, this, false);
         addView(mPanelPageIndicator);
+        updateSettings();
 
         ((PagedTileLayout) mTileLayout).setPageIndicator(mPanelPageIndicator);
         mQsTileRevealController = new QSTileRevealController(mContext, this,
@@ -713,9 +714,16 @@ public class QSPanel extends LinearLayout implements Tunable, Callback, Brightne
         int getOffsetTop(TileRecord tile);
 
         boolean updateResources();
+        void updateSettings();
 
         void setListening(boolean listening);
 
         default void setExpansion(float expansion) {}
+    }
+
+    public void updateSettings() {
+        if (mTileLayout != null) {
+            mTileLayout.updateSettings();
+        }
     }
 }
