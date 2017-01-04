1bf6d675b5da8cb0556c0fb0a2bac047e0796e5f
beanstown106 <nbenis106@gmail.com>
2017-01-04 13:29:17 -0500

    Pixel Navbar animation toggle [1/2]
    *made to work with the exposure for themers to disable navbar animation
    
    Change-Id: If15aff30447a65b9199d1768802ce86f1e1e1615
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4337,6 +4337,12 @@ public final class Settings {
         public static final String POWERMENU_LOGOUT = "powermenu_logout";
 
         /**
+         * Whether to enable the pixel navbar animation
+         * @hide
+         */
+        public static final String PIXEL_NAV_ANIMATION = "pixel_nav_animation";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
