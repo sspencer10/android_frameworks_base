2f84eeccc486d4834ccb8b3d5553cb9c33cd2ac7
maxwen <max.weninger@gmail.com>
2017-09-01 02:44:34 +0200

    base: add DeviceKeyHandler
    + Add key for proxi check for wake keys
    
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- core/java/com/android/internal/util/pixeldust/PixeldustUtils.java
+++ core/java/com/android/internal/util/pixeldust/PixeldustUtils.java
@@ -35,6 +35,7 @@ import android.os.PowerManager;
 import android.os.RemoteException;
 import android.os.ServiceManager;
 import android.os.SystemClock;
+import android.os.UserHandle;
 import android.view.InputDevice;
 import android.view.IWindowManager;
 import android.view.KeyCharacterMap;
@@ -215,5 +216,30 @@ public class PixeldustUtils {
             }
         }
         return null;
+    }
+
+    /**
+     * @hide
+     */
+    public static final String SYSTEMUI_PACKAGE_NAME = "com.android.systemui";
+
+    /**
+     * @hide
+     */
+    public static final String ACTION_DISMISS_KEYGUARD = SYSTEMUI_PACKAGE_NAME +".ACTION_DISMISS_KEYGUARD";
+
+    /**
+     * @hide
+     */
+    public static final String DISMISS_KEYGUARD_EXTRA_INTENT = "launch";
+
+    /**
+     * @hide
+     */
+    public static void launchKeyguardDismissIntent(Context context, UserHandle user, Intent launchIntent) {
+        Intent keyguardIntent = new Intent(ACTION_DISMISS_KEYGUARD);
+        keyguardIntent.setPackage(SYSTEMUI_PACKAGE_NAME);
+        keyguardIntent.putExtra(DISMISS_KEYGUARD_EXTRA_INTENT, launchIntent);
+        context.sendBroadcastAsUser(keyguardIntent, user);
     }
 }
