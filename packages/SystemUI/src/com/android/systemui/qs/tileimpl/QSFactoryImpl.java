f3d775e095aff4191262531a38dea4dabb8a124f
bigrushdog <randall.rushing@gmail.com>
2018-02-21 16:16:21 -0800

    Introduce Theme tile
    Long press tile to change between Style (dark/light/auto ui)
    mode and Accent (#FruityPebbles) mode.
    Click to show detail view.
    
    aex edits:- Include support for black and extended UI themes
                Update for Pie (ishubhamsingh)
    
    PixelDust Edits:
    * Add our supported themes: ShishuNights, Chocolate
    * Remove extended UI theme
    * Use our fancy accent colors:
      -BubblegumPink
      -DawnBlue
      -DryPaintMint
      -FootprintPurple
      -HeirloomBleu
      -ManiaAmber
      -SexyRed
      -SlateGray
      -SpookedPurple
      -Teal
      -UnexpectedBlueGray
    
    Change-Id: Ic9ca2a3305b4b79120d7ffca607477636fbbb5c5
    Signed-off-by: Shubham Singh <coolsks94@gmail.com>
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -51,6 +51,7 @@ import com.android.systemui.qs.tiles.ScreenshotTile;
 import com.android.systemui.qs.tiles.SoundTile;
 import com.android.systemui.qs.tiles.SmartPixelsTile;
 import com.android.systemui.qs.tiles.SyncTile;
+import com.android.systemui.qs.tiles.ThemeTile;
 import com.android.systemui.qs.tiles.UserTile;
 import com.android.systemui.qs.tiles.WifiTile;
 import com.android.systemui.qs.tiles.WorkModeTile;
@@ -137,6 +138,8 @@ public class QSFactoryImpl implements QSFactory {
                 return new AmbientDisplayTile(mHost);
             case "expanded_desktop":
                 return new ExpandedDesktopTile(mHost);
+            case "theme":
+                return new ThemeTile(mHost);
         }
 
         // Intent tiles.
