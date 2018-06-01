271a1c9ecd129495016a05b58b9dba4b662691f7
maxwen <max.weninger@gmail.com>
2018-06-01 17:41:07 +0200

    SystemUI: refactor quick settings expand fix
    not only for dt2s - make sure qs cant stay expanded once
    keyguard is started
    
    Change-Id: Ie8778544a529a93ccb0afb25f3a30a3ba50017dc
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
@@ -310,11 +310,6 @@ public class NotificationPanelView extends PanelView implements
                 PowerManager pm = (PowerManager) mContext.getSystemService(Context.POWER_SERVICE);
                 if(pm != null)
                     pm.goToSleep(e.getEventTime());
-                // quick pulldown can trigger those values
-                // on double tap - so reset them
-                mQsExpandImmediate = false;
-                requestPanelHeightUpdate();
-                setListening(false);
                 return true;
             }
         });
@@ -1198,6 +1193,7 @@ public class NotificationPanelView extends PanelView implements
             }
         }
         if (keyguardShowing) {
+            mQsExpandImmediate = false;
             updateDozingVisibilities(false /* animate */);
         }
         resetVerticalPanelPosition();
