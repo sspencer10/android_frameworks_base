bdf2bf90befee10e7046cc3c2a9cc6aafa27ded3
ZeNiXxX <vitjahermann@gmail.com>
2018-08-25 23:55:23 +0200

    SystemUI: Add Supsend Action Tile
    Created and brought to pie by ZeNiXxX
    
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -49,9 +49,10 @@ import com.android.systemui.qs.tiles.NightDisplayTile;
 import com.android.systemui.qs.tiles.RebootTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
 import com.android.systemui.qs.tiles.ScreenshotTile;
-import com.android.systemui.qs.tiles.SoundTile;
 import com.android.systemui.qs.tiles.SleepScreenTile;
 import com.android.systemui.qs.tiles.SmartPixelsTile;
+import com.android.systemui.qs.tiles.SoundTile;
+import com.android.systemui.qs.tiles.SuspendActionTile;
 import com.android.systemui.qs.tiles.SyncTile;
 import com.android.systemui.qs.tiles.ThemeTile;
 import com.android.systemui.qs.tiles.UserTile;
@@ -149,6 +150,8 @@ public class QSFactoryImpl implements QSFactory {
                 return new SleepScreenTile(mHost);
             case "compass":
                 return new CompassTile(mHost);
+            case "suspend_action":
+                return new SuspendActionTile(mHost);
         }
 
         // Intent tiles.
