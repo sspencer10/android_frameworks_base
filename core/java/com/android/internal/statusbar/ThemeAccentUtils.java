AospExtended/platform_frameworks_base

Use wellbeing overlay
Change-Id: Icbde7c86411954fb65318bb1eff4d8dcb76c68d5

core/java/com/android/internal/statusbar/ThemeAccentUtils.java
@@ -80,6 +80,7 @@ public class ThemeAccentUtils {
         "com.android.dialer.theme.extended", //4
         "com.android.contacts.theme.extended", //5
         "com.android.documentsui.theme.extended", //6
+        "com.google.android.apps.wellbeing.theme.extended", //7
     };
 
     private static final String[] CHOCOLATE_THEMES = {
@@ -90,6 +91,7 @@ public class ThemeAccentUtils {
         "com.android.dialer.theme.chocolate", //4
         "com.android.contacts.theme.chocolate", //5
         "com.android.documentsui.theme.chocolate", //6
+        "com.google.android.apps.wellbeing.theme.chocolate", //7
     };
 
 

sspencer10/android_frameworks_base

Signed-off-by: spezi77 <spezi7713@gmx.net>

core/java/com/android/internal/statusbar/ThemeAccentUtils.java
@@ -82,6 +82,7 @@ public class ThemeAccentUtils {
         "com.android.dialer.theme.shishunights", //4
         "com.android.contacts.theme.shishunights", //5
         "com.android.documentsui.theme.shishunights", //6
+        "com.google.android.apps.wellbeing.theme.shishunights", //7
     };
 
     private static final String[] CHOCOLATE_THEMES = {
@@ -92,6 +93,7 @@ public class ThemeAccentUtils {
         "com.android.dialer.theme.chocolate", //4
         "com.android.contacts.theme.chocolate", //5
         "com.android.documentsui.theme.chocolate", //6
+        "com.google.android.apps.wellbeing.theme.chocolate", //7
     };
 
     private static final String STOCK_DARK_THEME = "com.android.systemui.theme.dark";